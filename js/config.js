ac7a178fafe02c39a46e93dc916913b490112fb1
Seoman76 <98778900+Seoman76@users.noreply.github.com>
2026-08-30 22:26:21 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,11 +4,11 @@ const CONFIG = {
 
     sheets: {
 
-        jojo: "HIER_DEN_CSV_LINK_VON_JOJO_EINFÜGEN",
+        jojo: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR-l95auLVcne0kWg1Q5_tDhvXXpLeAUDtw_lHix1D_TyaJfSXHBdV6PZZ8DkHK6h_PqvWRtj7A5Vuf/pub?gid=0&single=true&output=csv",
 
-        jooris: "HIER_DEN_CSV_LINK_VON_JOORIS_EINFÜGEN",
+        jooris: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR-l95auLVcne0kWg1Q5_tDhvXXpLeAUDtw_lHix1D_TyaJfSXHBdV6PZZ8DkHK6h_PqvWRtj7A5Vuf/pub?gid=1233781474&single=true&output=csv,
 
-        jule: "HIER_DEN_CSV_LINK_VON_JULE_EINFÜGEN"
+        jule: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR-l95auLVcne0kWg1Q5_tDhvXXpLeAUDtw_lHix1D_TyaJfSXHBdV6PZZ8DkHK6h_PqvWRtj7A5Vuf/pub?gid=175444478&single=true&output=csv"
 
     }
 
