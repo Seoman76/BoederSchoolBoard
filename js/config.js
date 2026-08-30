87564c6c5ffe2099125703ffd02bb2abe6476ef0
Seoman76 <98778900+Seoman76@users.noreply.github.com>
2026-08-30 18:29:01 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,30 @@
+/* ==========================================
+   Böder SchoolBoard
+   Version 0.2
+   Konfiguration
+========================================== */
 
+const CONFIG = {
+
+    appName: "Böder SchoolBoard",
+
+    refreshInterval: 60000, // Aktualisierung alle 60 Sekunden
+
+    sheets: {
+
+        jojo: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR-l95auLVcne0kWg1Q5_tDhvXXpLeAUDtw_IHix1D_TyaJfSXHBdV6PZZ8DkHK6h_PgvWRtj7A5Vuf/pub?gid=0&single=true&output=csv",
+
+        jooris: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR-l95auLVcne0kWg1Q5_tDhvXXpLeAUDtw_IHix1D_TyaJfSXHBdV6PZZ8DkHK6h_PgvWRtj7A5Vuf/pub?gid=1233781474&single=true&output=csv",
+
+        jule: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR-l95auLVcne0kWg1Q5_tDhvXXpLeAUDtw_IHix1D_TyaJfSXHBdV6PZZ8DkHK6h_PgvWRtj7A5Vuf/pub?gid=175444478&single=true&output=csv"
+
+    },
+
+    weather: {
+
+        enabled: false,
+        city: "Marl"
+
+    }
+
+};
