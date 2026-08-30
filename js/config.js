afbcd1cddca726e66a1d820a2f2d54f3491abdea
Seoman76 <98778900+Seoman76@users.noreply.github.com>
2026-08-30 22:23:55 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,15 @@
-const SHEET_ID="1ravA7izByD4IN7aUhxsMNQ7mn185jvYYFLlQUnAXFDM";
+const CONFIG = {
+
+    refreshInterval: 60000,
+
+    sheets: {
+
+        jojo: "HIER_DEN_CSV_LINK_VON_JOJO_EINFÜGEN",
+
+        jooris: "HIER_DEN_CSV_LINK_VON_JOORIS_EINFÜGEN",
+
+        jule: "HIER_DEN_CSV_LINK_VON_JULE_EINFÜGEN"
+
+    }
+
+};
