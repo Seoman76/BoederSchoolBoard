5375d928748762912af82b42816e819fa551b6c2
Seoman76 <98778900+Seoman76@users.noreply.github.com>
2026-09-02 12:53:39 +0200

    Update clock.js
    v 0.2.3

--- js/clock.js
+++ js/clock.js
@@ -1,38 +1,37 @@
 /* ==========================================
    Böder SchoolBoard
-   Version 0.2.2
-   Uhr & Datum
+   Version 0.2.3 Foundation
 ========================================== */
 
-function updateClock() {
+function updateClock(){
 
     const now = new Date();
 
     const clock = document.getElementById("clock");
     const date = document.getElementById("date");
 
-    const time = now.toLocaleTimeString("de-DE", {
-        hour: "2-digit",
-        minute: "2-digit"
-    });
+    if(clock){
 
-    const today = now.toLocaleDateString("de-DE", {
-        weekday: "long",
-        day: "2-digit",
-        month: "long",
-        year: "numeric"
-    });
+        clock.textContent = now.toLocaleTimeString("de-DE",{
+            hour:"2-digit",
+            minute:"2-digit"
+        });
 
-    if (clock) {
-        clock.textContent = time;
     }
 
-    if (date) {
-        date.textContent = today;
+    if(date){
+
+        date.textContent = now.toLocaleDateString("de-DE",{
+            weekday:"long",
+            day:"2-digit",
+            month:"long",
+            year:"numeric"
+        });
+
     }
 
 }
 
 updateClock();
 
-setInterval(updateClock, 1000);
+setInterval(updateClock,1000);
