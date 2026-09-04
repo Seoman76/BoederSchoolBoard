380347bd774cf0669654b8da12ad1f6ccde833f4
Seoman76 <98778900+Seoman76@users.noreply.github.com>
2026-09-04 13:39:14 +0200

    Update Sheets.js

--- js/Sheets.js
+++ js/Sheets.js
@@ -1,31 +1,19 @@
 /* ==========================================
+   Böder SchoolBoard
+   Version 0.2
    Google Sheets Loader
 ========================================== */
 
-async function loadSheet(url){
+async function loadSheet(url) {
 
-    try{
+    const response = await fetch(url);
 
-        const response = await fetch(url);
-
-        if(!response.ok){
-
-            throw new Error("CSV konnte nicht geladen werden.");
-
-        }
-
-        const csv = await response.text();
-
-        return parseCSV(csv);
-
+    if (!response.ok) {
+        throw new Error("Google Sheet konnte nicht geladen werden.");
     }
 
-    catch(error){
+    const text = await response.text();
 
-        console.error(error);
-
-        return null;
-
-    }
+    return text;
 
 }
